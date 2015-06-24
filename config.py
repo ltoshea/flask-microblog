d138398809504a609847a183428e3cb82ffb27da
Azureaus <liam.oshea@o2-v2.com>
2015-06-24 14:16:08 +0800

    Added models and db scripts

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+import os 
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(basedir,'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir,'db_repo')
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 OPENID_PROVIDERS = [
